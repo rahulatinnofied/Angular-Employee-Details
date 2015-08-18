49e627cc65c770521edd5e3fcb2e220ab421ad58
Rahul <rahul.karmakar@innofied.com>
2015-08-18 13:35:21 +0530

    Initial Commit

---
+++ js/template.js
@@ -0,0 +1,6 @@
+'use strict';
+angular.module('myApp', [])
+    .controller('myController', ['$scope', function($scope) {
+        $scope.months = ['jan', 'feb', 'mar', 'apr', 'may', 'june', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec'];
+        $scope.years = [2010, 2011, 2012, 2013, 2014, 2015];
+    }]);
